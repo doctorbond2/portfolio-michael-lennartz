d65a353be82ae7106d5b8fae3accd2b4a0ae65f9
doctorbond2 <michael.rostlund@hotmail.com>
2024-09-25 15:18:23 +0200

    Refactor HeaderLayout component to remove console logs and unused code

--- src/components/server/Projects/ProjectsHeader.tsx
+++ src/components/server/Projects/ProjectsHeader.tsx
@@ -73,50 +73,5 @@ export default function ProjectsHeader({ handleSelection }: Props) {
         </div>
       </div>
     </div>
-
-    // <div
-    //   className="md:w-[18%] md:ml-[5%] md:mr-auto z-20 relative"
-    //   ref={dropdownRef}
-    // >
-    //   <button
-    //     onClick={toggleMenu}
-    //     className="font-bold w-fit  border border-black text-white bg-black rounded-md p-1 hover:bg-white hover:text-black"
-    //   >
-    //     Projects Menu
-    //   </button>
-    //   <div
-    //     className={`absolute left-0 right-0 mt-2 bg-white border-2 border-black transition-all duration-500 ease-in-out rounded-md ${
-    //       isMenuOpen
-    //         ? 'opacity-100 max-h-[20rem] pointer-events-auto'
-    //         : 'opacity-0 max-h-0 pointer-events-none'
-    //     }`}
-    //   >
-    //     <div className="flex flex-col mt-2 font-nanum font-bold text-lg">
-    //       <button
-    //         onClick={(e) => {
-    //           handleSelection(e);
-    //           toggleMenu();
-    //         }}
-    //         value={''}
-    //         className="mx-auto my-1 underlineAnimation"
-    //       >
-    //         All projects
-    //       </button>
-    //
-    //         <button
-    //           key={0}
-    //           value={ 'placeholder' }
-    //           onClick={(e) => {
-    //             handleSelection(e);
-    //             toggleMenu();
-    //           }}
-    //           className="underlineAnimation mx-auto my-1"
-    //         >
-    //           placeholder
-    //         </button>
-    //
-    //     </div>
-    //   </div>
-    // </div>
   );
 }
